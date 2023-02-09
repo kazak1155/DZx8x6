9961daad9751643dd65c77bcd530cc9c1d2a3e0e
Andrey Volynkin <kazakmt4@gmail.com>
2023-02-09 10:02:55 +0300

    111

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Main Page Laravel</title>
+    </head>
+    <body>
+     8.6 Практическая работа
+    </body>
+</html>
